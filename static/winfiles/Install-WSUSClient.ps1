1f4aa75c893a5c7e73f3469d76f2392ab8b7604b
Tobias Löb <tobias_loeb@web.de>
2016-03-18 11:02:13 +0100

    ADDED Script for initializing WSUS Clients

--- static/winfiles/Install-WSUSClient.ps1
+++ static/winfiles/Install-WSUSClient.ps1
@@ -1,1 +1,4 @@
-Set-ClientWSUSSetting -Options DownloadOnly -DetectionFrequency 4 -UpdateServer "http://wsus" -Verbose
+Set-ClientWSUSSetting -Options DownloadOnly -DetectionFrequency 4 -UpdateServer "http://192.168.0.2" -Verbose
+
+
+https://technet.microsoft.com/en-us/library/cc708449%28WS.10%29.aspx
